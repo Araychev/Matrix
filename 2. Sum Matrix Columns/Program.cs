bf34dc7b33c8230d26ac802e6d0f6aa173336631
Angel Raychev <angelraychev@gmail.com>
2021-06-14 21:00:45 +0300

    New

---
+++ 2. Sum Matrix Columns/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Sum_Matrix_Columns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowCol = ReadArrayFromConsole();
+
+            int rows = rowCol[0];
+            int cols = rowCol[1];
+
+            int[,] matrix = FillMatrix(rows, cols);
+            PrintCollSum(matrix);
+        }
+
+        static void PrintCollSum(int[,] matrix)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sum += matrix[row, col];
+                }
+                Console.WriteLine(sum);
+
+            }
+        }
+
+        static int[,] FillMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colElements = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+            return matrix;
+        }
+
+        static int[] ReadArrayFromConsole()
+        {
+            return Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
---
+++ Exersize/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace Exersize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowCol = Console.ReadLine()
+                 .Split(", ")
+                 .Select(int.Parse)
+                 .ToArray();
+
+            int row1 = rowCol[0];
+            int col1 = rowCol[1];
+
+            int[,] matrix = new int[row1, col1];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colElements = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    sum += matrix[row, col];
+                }
+            }
+            Console.WriteLine(row1);
+            Console.WriteLine(col1);
+            Console.WriteLine(sum);
+            
+            
+        }
+
+
+    }
+        
+    
+}
